af519a6b3255e69d277a557222bb94479f28587f
Kareem Mohamed <61213045+KareemZafan@users.noreply.github.com>
2026-01-03 18:44:04 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
+double add (double a, double b){
+
+ return a + b ; 
+}
 int main(){
 
  printf("Hello World"); 
+
+ double a = 10; 
+ double b = 20; 
+ printf("Sum of (%f,%f) = %f,a,b , add(a+b))   // Sum(10,20) = 30
 
 return 0; 
 }
