e2087a4b67f325a039bf645f01e923fa1d9b0bd5
Kareem Mohamed <kareem.mohamed@adfinis.com>
2026-01-03 19:19:18 +0200

    After resetting head

--- src/file.py
+++ src/file.py
@@ -1,22 +1,19 @@
-
-### Python file 
+### Python file
 print("Hello")
 
 names = ["Ahmed", "Mohamed", "Alaa"]
 
-
-
-## print names 
+## print names
 for i in names:
-  print(i)
-  
-  
-print_name("Ahmed)  
-  
-  
+    print(i)
+
+
 def print_name(name):
-	print(f"Hello {name}") 
+    print(f"Hello {name}")
 
 
-def add (a , b): 
- return a + b
+def add(a, b):
+    return a + b
+
+
+print_name("Ahmed")
